55c4268909e4a7d177af788f054b597524bb98eb
Chintan <chihempat@gmail.com>
2021-03-02 12:08:17 +0530

    added project

---
+++ Nodejs starting/BackEndNodeJS-master/Node Practise/finalproject/index.js
@@ -0,0 +1,44 @@
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+
+//bring all routes
+const auth = require('./routes/api/auth');
+const question = require('./routes/api/questions');
+const profile = require('./routes/api/profile');
+
+
+
+
+//middleware
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+//mongodb configuration
+const db = require('./setup/myurl').mongoURL
+
+//attempt to connect ot DB
+mongoose.
+connect(db)
+    .then(() => console.log('Connected'))
+    .catch((err) => console.log(err));
+
+
+//just for testing
+app.get('/', (req, res) => {
+    res.send("Hello")
+});
+
+//actual routes
+app.use('/api/auth', auth)
+app.use('/api/profile', profile)
+app.use('/api/questions', question)
+
+app.listen(port, () => {
+    console.log("Appp running at 3000")
+});
---
+++ Nodejs starting/BackEndNodeJS-master/Node Practise/finalproject/setup/myurl.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURL: "mongodb+srv://Chintan:helloworld@cluster0.w12fo.mongodb.net/bigstack?retryWrites=true&w=majority",
+    secret: "secret"
+}

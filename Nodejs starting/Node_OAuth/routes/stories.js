67291fd42f628dbfe098a8421fbbb5001ada0736
Chintan <chihempat@gmail.com>
2021-03-17 17:30:22 +0530

    done

--- Nodejs starting/Node_OAuth/routes/stories.js
+++ Nodejs starting/Node_OAuth/routes/stories.js
@@ -124,4 +124,39 @@ router.delete("/:id", ensureAuth, async(req, res) => {
         return res.render('error/500')
     }
 })
+
+//@ type Get
+//@route /stories/user/:userId
+//@desc To fetch user stories
+//@access PRIVATE
+router.get("/user/:userId", ensureAuth, async(req, res) => {
+    try {
+        const stories = await Story.find({ user: req.params.userId, status: 'public' }).populate("user").lean()
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500')
+    }
+})
+
+// router.get('/user/:userId', ensureAuth, async(req, res) => {
+//     try {
+//         const stories = await Story.find({
+//                 user: req.params.userId,
+//                 status: 'public',
+//             })
+//             .populate('user')
+//             .lean()
+
+//         res.render('stories/index', {
+//             stories,
+//         })
+//     } catch (err) {
+//         console.error(err)
+//         res.render('error/500')
+//     }
+// })
+
 module.exports = router;

86cb0dd30f554d637f5e55ba03c4e968a55a9721
Chintan <chihempat@gmail.com>
2021-03-17 17:11:23 +0530

    added show render

--- Nodejs starting/Node_OAuth/routes/stories.js
+++ Nodejs starting/Node_OAuth/routes/stories.js
@@ -44,31 +44,52 @@ router.get('/', ensureAuth, async(req, res) => {
     }
 })
 
-//@ type Get
-//@route /edit/id
-//@desc To ediit
+//@ type GET
+//@route /id
+//@desc To get all the stories
 //@access PRIVATE
-router.get('/edit/:id', ensureAuth, async(req, res) => {
-    try {
-        const story = await Story.findOne({ _id: req.params.id }).lean()
-        if (!story) {
-            return res.render('error/404')
-        }
-
-        if (story.user != req.user.id) {
-            res.redirect('/stories')
-        } else {
-            res.render('stories/edit', {
+router.get('/:id', ensureAuth, async(req, res) => {
+        try {
+            let story = await Story.findById(req.params.id).populate('user').lean()
+            if (!story) {
+                res.render('error/404')
+            }
+            res.render('stories/show', {
                 story,
             })
+        } catch (err) {
+            console.error(err)
+            res.render('error/404')
         }
-    } catch (err) {
-        console.error(err)
-        return res.render('error/500')
-    }
+    })
+    //@ type Get
+    //@route /edit/id
+    //@desc To get to edit page
+    //@access PRIVATE
+router.get('/edit/:id', ensureAuth, async(req, res) => {
+        try {
+            const story = await Story.findOne({ _id: req.params.id }).lean()
+            if (!story) {
+                return res.render('error/404')
+            }
 
-})
+            if (story.user != req.user.id) {
+                res.redirect('/stories')
+            } else {
+                res.render('stories/edit', {
+                    story,
+                })
+            }
+        } catch (err) {
+            console.error(err)
+            return res.render('error/500')
+        }
 
+    })
+    //@ type Get
+    //@route /id
+    //@desc To edit story
+    //@access PRIVATE
 router.put('/:id', ensureAuth, async(req, res) => {
     try {
         let story = await Story.findById(req.params.id)
@@ -86,6 +107,20 @@ router.put('/:id', ensureAuth, async(req, res) => {
         }
     } catch (err) {
         console.error(err)
+        return res.render('error/500')
+    }
+})
+
+//@ type Get
+//@route /id
+//@desc To delete story
+//@access PRIVATE
+router.delete("/:id", ensureAuth, async(req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err);
         return res.render('error/500')
     }
 })
